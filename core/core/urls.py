c91ad0d2cbd928181ff4e6afad11d7b8d8a7a5c5
vramirez209 <vramirez209@gmail.com>
2020-10-29 23:36:24 -0400

    test folder structure

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,4 +1,4 @@
-"""natic URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
